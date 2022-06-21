105ab8d90d27ec0c9773c56c6a49b0bc0b735e26
Danil Ratnikov <feed111feed111@mail.ru>
2022-06-21 15:39:06 +0700

    123

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,1 +1,18 @@
-print("Hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+ return " <html><head></head> <body> Hello World! </body></html>"
+
+if __name__ == "__main__":
+ app.run()
+
+
+'''
+pip install waitress
+waitress-serve --listen=127.0.0.1:5000 wsgi:app
+
+gunicorn --bind 127.0.0.1:5000 wsgi:app
+'''
