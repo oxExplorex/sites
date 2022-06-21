a065502dff86777de4bd7a97f86cb7dfc479dffb
Danil Ratnikov <feed111feed111@mail.ru>
2022-06-21 15:49:12 +0700

    1232311

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+@app.route("/data_to")
+def data_to():
+  some_pars = {'user': 'Ivan', 'color': 'red'}
+  some_str = 'Hello my dear friends!'
+  some_value = 10
+  return render_template('simple.html', some_str=some_str, some_value=some_value, some_pars=some_pars)
+
+
 @app.route("/")
 def hello():
- return " <html><head></head> <body> Hello World! </body></html>"
+  return " <html><head></head> <body> Hello World! </body></html>"
 
 if __name__ == "__main__":
- app.run()
+  app.run()
 
 
 '''
